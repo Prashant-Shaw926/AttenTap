54c2b0337a73b091c88276766c8ccfcb22acfc4d
Prashant Shaw <prashant.shaw926@gmail.com>
2026-04-16 20:34:38 +0530

    feat: implement React Navigation and setup basic AppNavigator with HomeScreen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,1 +1,28 @@
 
+import * as React from 'react';
+import { StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createStackNavigator();
+
+function RootStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+    </Stack.Navigator>
+  );
+}
+
+export default function AppNavigator() {
+  return (
+    <NavigationContainer>
+      <RootStack />
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+
+})
